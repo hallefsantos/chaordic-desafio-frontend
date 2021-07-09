1b187a18cf94af02b03ba0d77c565e76d44cc4b1
Hallef Santos <hallefsantos@gmail.com>
2021-07-09 17:13:35 -0300

    Add link to home on Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className="py-5 md:py-14 text-white bg-gradient-to-r from-gray-800 to-gray-600">
       <div className="w-full max-w-7xl mx-auto px-4">
         <div>
-          <a className="text-2xl md:text-4xl font-bold" href="#">Breweries</a>
+          <Link href="/">
+            <a className="text-2xl md:text-4xl font-bold">Breweries</a>
+          </Link>
         </div>
         <div className="mt-0 md:mt-4 text-base md:text-xl">A breweries list by Open Brewery DB</div>
       </div>
