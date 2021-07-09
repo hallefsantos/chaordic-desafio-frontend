635230e388d8ad85d96a61d32911c1cd86c435cf
Hallef Santos <hallefsantos@gmail.com>
2021-07-08 21:59:53 -0300

    Update TailwindCss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  mode: 'jit',
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Roboto Slab', ...defaultTheme.fontFamily.sans],
+    },
+
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    container: false,
+  }
 }
