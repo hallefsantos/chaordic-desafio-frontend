b4dd71b190301f12e55647c90ef1aa45cf02b980
Hallef Santos <hallefsantos@gmail.com>
2021-07-08 22:21:27 -0300

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="py-5 md:py-14 text-white bg-gradient-to-r from-gray-800 to-gray-600">
+      <div className="w-full max-w-7xl mx-auto px-4">
+        <div>
+          <a className="text-2xl md:text-4xl font-bold" href="#">Breweries</a>
+        </div>
+        <div className="mt-0 md:mt-4 text-base md:text-xl">A breweries list by Open Brewery DB</div>
+      </div>
+      
+    </header>
+  )
+}
+
+export default Header
